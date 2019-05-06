6fffa48678bfacce0321cd9f813d21f02c717256
liushuai <1308956271@qq.com>
2019-05-06 22:07:11 +0800

    【commit 互动项目架构搭建】

---
+++ src/Interact.WebApp/Interact.Infrastructure/Config/Model/WeixinConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interact.Infrastructure.Config.Model
+{
+    /// <summary>
+    /// 微信配置相关
+    /// </summary>
+    public class WeixinConfig
+    {
+        public  static string AppKey=>ConfigurationManager.AppSettings["weixin_appkey"];
+        public static string AppSecuret=>ConfigurationManager.AppSettings["weixin_appkey"];
+
+    }
+}
