aaa5e30418640a2952e390ff812ae3441c6fdadb
liushuai <1308956271@qq.com>
2019-05-07 23:31:56 +0800

    【工具类】

---
+++ src/Interact.WebApp/Interact.Infrastructure/Helper/CookieHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Interact.Infrastructure.Helper
+{
+    /// <summary>
+    /// cookie帮助类
+    /// </summary>
+   public class CookieHelper
+    {
+        public static void Set(string value,string name,TimeSpan expires)
+        {
+            HttpCookie cookie = new HttpCookie(name, value) {
+                 Expires=DateTime.Now.Add(expires)
+            };
+           
+            HttpContext.Current.Response.Cookies.Add(cookie);
+        }
+        public static string Get(string name)
+        {
+           HttpCookie cookie= HttpContext.Current.Response.Cookies.Get(name);
+            if (cookie == null) return string.Empty;
+            return cookie.Value;
+        }
+    }
+}
--- src/Interact.WebApp/Interact.Infrastructure/Helper/JWTHelper.cs
+++ src/Interact.WebApp/Interact.Infrastructure/Helper/JWTHelper.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Interact.Infrastructure.Config.Model;
+using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,49 @@ using System.Threading.Tasks;
 
 namespace Interact.Infrastructure.Helper
 {
-    class JWTHelper
+
+    /// <summary>
+    /// JWT帮助类
+    /// </summary>
+    public class JWTHelper
     {
+        /// <summary>
+        /// 生成JwtToken
+        /// </summary>
+        /// <param name="payload">不敏感的用户数据</param>
+        /// <returns></returns>
+        public static string Set(Dictionary<string, object> payload)
+        {
+
+            //格式如下
+            //var payload = new Dictionary<string, object>
+            //{
+            //    { "username","admin" },
+            //    { "pwd", "claim2-value" }
+            //};
+
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+            var token = encoder.Encode(payload, WebConfig.JWT_Secret);
+            return token;
+        }
+
+        /// <summary>
+        /// 根据jwtToken  获取实体
+        /// </summary>
+        /// <param name="token">jwtToken</param>
+        /// <returns></returns>
+        public static T GetJwtDecode<T>(string token)
+        {
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IDateTimeProvider provider = new UtcDateTimeProvider();
+            IJwtValidator validator = new JwtValidator(serializer, provider);
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder);
+            var userInfo = decoder.DecodeToObject<T>(token, WebConfig.JWT_Secret, verify: true);//token为之前生成的字符串
+            return userInfo;
+        }
     }
 }
