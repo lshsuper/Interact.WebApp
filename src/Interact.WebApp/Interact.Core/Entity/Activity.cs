3a501c73d3623e6d14f132c14534afa8197aa97a
liushuai <liushuai@adzshd.com>
2019-05-09 10:17:32 +0800

    【commit 模型定义】

---
+++ src/Interact.WebApp/Interact.Core/Entity/Activity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interact.Core.Entity
+{
+  public  class Activity
+    {
+        /// <summary>
+        /// 活动id
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 活动名称
+        /// </summary>
+        public string Nanme { get; set; }
+        /// <summary>
+        /// 报名限制
+        /// </summary>
+        public int LimitNumber { get; set; }
+        /// <summary>
+        /// 海报地址
+        /// </summary>
+        public string PlayBillUrl { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ src/Interact.WebApp/Interact.Core/Entity/WinnerMenu.cs
@@ -0,0 +1,37 @@
+﻿using Interact.Core.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interact.Core.Entity
+{
+    /// <summary>
+    /// 中奖人名单
+    /// </summary>
+   public class WinnerMenu
+    {
+        /// <summary>
+        /// 记录id
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// 对应签到记录id
+        /// </summary>
+        public string SiginInRecoredId { get; set; }
+        /// <summary>
+        /// 活动id
+        /// </summary>
+        public int ActivityId { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// 中奖等级（暂时用枚举写死，可扩展成单独表）
+        /// </summary>
+        public WinnerLevelEnum WinnerLevel { get; set; }
+
+    }
+}
