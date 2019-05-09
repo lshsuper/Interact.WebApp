5263a26edeb28aa22c119f24745720dd1197da1c
liushuai <liushuai@adzshd.com>
2019-05-09 14:01:06 +0800

    【commit 建库--生成sql】

--- src/Interact.WebApp/Interact.Core/Entity/Activity.cs
+++ src/Interact.WebApp/Interact.Core/Entity/Activity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Interact.Core.Enum;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,5 +33,9 @@ namespace Interact.Core.Entity
         /// 创建时间
         /// </summary>
         public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// 活动状态
+        /// </summary>
+        public ActivityStatusEnum Status { get; set; }
     }
 }
