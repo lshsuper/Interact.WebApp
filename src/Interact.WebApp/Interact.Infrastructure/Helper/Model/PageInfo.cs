d8fb708468cd893abadaf722e44bc78cfda507ec
liushuai <liushuai@adzshd.com>
2019-05-09 18:46:14 +0800

    【commit 接口补充】

---
+++ src/Interact.WebApp/Interact.Infrastructure/Helper/Model/PageInfo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interact.Infrastructure.Helper.Model
+{
+    public class PageInfo<T>
+    {
+        #region Compute Attr
+        /// <summary>
+        /// 当前页码
+        /// </summary>
+
+        public int PageIndex
+        {
+            get; set;
+        }
+        public int PageCount
+        {
+            get
+            {
+                if (DataCount <= 0)
+                {
+                    return 0;
+                }
+               
+                int pageCount = Convert.ToInt32(Math.Ceiling((double)DataCount / pageSize));
+                return pageCount;
+            }
+        }
+        /// <summary>
+        /// 每页条数
+        /// </summary>
+        private int pageSize;
+        public int PageSize
+        {
+            get
+            {
+                return pageSize;
+            }
+            set
+            {
+                if (pageSize <= 0)
+                {
+                    pageSize = 10;
+                }
+                else
+                {
+                    pageSize = value;
+                }
+            }
+        }
+
+        #endregion
+       
+        public T DataSource { get; set; }
+        public int DataCount { get; set; } = 0;
+    }
+}
