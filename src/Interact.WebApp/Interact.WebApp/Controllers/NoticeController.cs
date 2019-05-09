b0f38c8a346a7232b5429d1ab4c29487efdd0ea4
liushuai <liushuai@adzshd.com>
2019-05-09 19:07:52 +0800

    【commit 提取提示页】

--- src/Interact.WebApp/Interact.WebApp/Controllers/NoticeController.cs
+++ src/Interact.WebApp/Interact.WebApp/Controllers/NoticeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Interact.WebApp.Areas.Home.Controllers
+namespace Interact.WebApp.Controllers
 {
     public class NoticeController : Controller
     {
