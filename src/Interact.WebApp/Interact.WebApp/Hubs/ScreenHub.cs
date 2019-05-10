3ad9e2b667ce70a6153d645b6d844de23c350029
liushuai <liushuai@adzshd.com>
2019-05-10 18:19:29 +0800

    【commit Ticker整合】

--- src/Interact.WebApp/Interact.WebApp/Hubs/ScreenHub.cs
+++ src/Interact.WebApp/Interact.WebApp/Hubs/ScreenHub.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
 namespace Interact.WebApp.Hubs
 {
+    /// <summary>
+    /// 大屏幕事件总线
+    /// </summary>
     public class ScreenHub : Hub
     {
-        public void Hello()
+        /// <summary>
+        /// 断开连接触发的事件
+        /// </summary>
+        /// <param name="stopCalled"></param>
+        /// <returns></returns>
+        public override Task OnDisconnected(bool stopCalled)
         {
-            Clients.All.hello();
+            return base.OnDisconnected(stopCalled);
+        }
+        /// <summary>
+        /// 连接上时触发的事件
+        /// </summary>
+        /// <returns></returns>
+        public override Task OnConnected()
+        {
+            return base.OnConnected();  
         }
     }
 }
---
+++ src/Interact.WebApp/Interact.WebApp/Models/ScreenTicker.cs
@@ -0,0 +1,47 @@
+﻿using Interact.WebApp.Hubs;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using System.Web;
+
+namespace Interact.WebApp.Models
+{
+    /// <summary>
+    /// 消息收发机
+    /// </summary>
+    public class ScreenTicker
+    {
+        // Singleton instance
+        private readonly static Lazy<ScreenTicker> _instance = new Lazy<ScreenTicker>(() => new ScreenTicker(GlobalHost.ConnectionManager.GetHubContext<ScreenHub>().Clients));
+        private ScreenTicker(IHubConnectionContext<dynamic> clients)
+        {
+            Clients = clients;
+        
+        }
+        public static ScreenTicker Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+        /// <summary>
+        /// 所有客户端
+        /// </summary>
+        private IHubConnectionContext<dynamic> Clients
+        {
+            get;
+            set;
+        }
+
+        public void Test()
+        {
+            Clients.All.hello();
+        }
+       
+    }
+}
