76fe30d8dfcc268b53cd8a7671eaf33996aa6d32
liushuai <liushuai@adzshd.com>
2019-05-10 17:16:42 +0800

    【commit signalr的整合】

---
+++ src/Interact.WebApp/Interact.WebApp/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Interact.WebApp.Startup))]
+
+namespace Interact.WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //应用signalr通信
+            app.MapSignalR();
+            // 有关如何配置应用程序的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
