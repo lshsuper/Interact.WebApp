88368b97228d27cde4d7ed67d2063a009d7d42b6
liushuai <liushuai@adzshd.com>
2019-05-14 18:35:47 +0800

    xxx

---
+++ src/Interact.WebApp/Interact.Infrastructure/WeChat/Model/WeChatJsapiTicketResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interact.Infrastructure.WeChat.Model
+{
+    /// <summary>
+    /// jsjdk-ticket返回结果模型
+    /// </summary>
+    public class WeChatJsapiTicketResult
+    {
+        /*成功result
+         {
+            "errcode":0,
+            "errmsg":"ok",
+            "ticket":"bxLdikRXVbTPdHSM05e5u5sUoXNKd8-41ZO3MhKoyN5OfkWITDGgnr2fwJ0m9E8NYzWKVZvdVtaUgWvsdshFKA",
+            "expires_in":7200
+          }
+         */
+        public string errcode { get; set; }
+        public string errmsg { get; set; }
+        public string ticket { get; set; }
+        public string expires_in { get; set; }
+    }
+}
---
+++ src/Interact.WebApp/Interact.Infrastructure/WeChat/WeChatHelper.cs
@@ -0,0 +1,32 @@
+﻿using Interact.Infrastructure.Helper;
+using Interact.Infrastructure.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interact.Infrastructure.WeChat
+{
+  public  class WeChatHelper
+    {
+        /// <summary>
+        /// js-sdk签名
+        /// </summary>
+        /// <param name="jsapi_ticket">jsapi_ticket</param>
+        /// <param name="noncestr">随机字符串(必须与wx.config中的nonceStr相同)</param>
+        /// <param name="timestamp">时间戳(必须与wx.config中的timestamp相同)</param>
+        /// <param name="url">当前网页的URL，不包含#及其后面部分(必须是调用JS接口页面的完整URL)</param>
+        /// <returns></returns>
+        public static string GetSignature(string jsapi_ticket, string noncestr, long timestamp, string url, out string str)
+        {
+            var sb = new StringBuilder();
+            sb.Append("jsapi_ticket=").Append(jsapi_ticket).Append("&")
+                          .Append("noncestr=").Append(noncestr).Append("&")
+                          .Append("timestamp=").Append(timestamp).Append("&")
+                          .Append("url=").Append(url.IndexOf("#") >= 0 ? url.Substring(0, url.IndexOf("#")) : url);
+            str = sb.ToString();
+            return str.Sha1();
+        }
+    }
+}
