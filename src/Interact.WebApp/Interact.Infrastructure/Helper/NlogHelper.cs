f664f2b192916f07259d14b0497ab5991eda0f74
liushuai <1308956271@qq.com>
2019-05-16 22:26:03 +0800

    【commit】

---
+++ src/Interact.WebApp/Interact.Infrastructure/Helper/NlogHelper.cs
@@ -0,0 +1,38 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interact.Infrastructure.Helper
+{
+   public class NlogHelper
+    {
+        private static NLog.ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        public enum LogLevel
+        {
+            Debug,
+            Info,
+            Error,
+            Fatal,
+            Warn,
+
+        }
+        #region 
+        public static void LogWrite(LogLevel level)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+    }
+}
